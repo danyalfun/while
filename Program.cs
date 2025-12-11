7a9acec0bc1c8023cce4308c50e554cc7ef91e0e
danyalfun <danyal.fun@gmail.com>
2025-12-11 09:28:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Task1()
+        {
+            int num;
+            int cunt = 0;
+            while (cunt != 3)
+            {
+                Console.WriteLine("enter a number");
+                num = int.Parse(Console.ReadLine());
+                if (num%2 == 0)
+                    cunt++ ;
+            }
+            Console.WriteLine("finally");
+                
+        }
+        static void Task2()
+        {
+            int sam = 0;
+            int pr;
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            while (sam < 100)
+            {
+                Console.WriteLine("enter a price :");
+                pr = int.Parse(Console.ReadLine());
+                sam = sam + pr;
+                if (min > pr || sam == 0) { min = pr; }
+                if (max < pr || sam == 0) { max = pr; }
+            }
+            Console.WriteLine("totl sam: " + sam);
+            Console.WriteLine("min: " + min);
+            Console.WriteLine("max: " + max);
+        }
+        static void Task3()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            //Task1();
+            Task2();
+            Task3();
+        }
+    }
+}
